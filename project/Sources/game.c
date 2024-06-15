9f59a63512081c699780d7b7cf6fd7b24f3fdbde
Gustavo Soares <gusnsoares@gmail.com>
2024-06-15 10:04:12 -0300

    state machine

--- project/Sources/game.c
+++ project/Sources/game.c
@@ -8,17 +8,42 @@ void game_loop(uint8_t sets_to_win) {
     board_t board;
     float t1, t2;
     board_init(&board);
+    t1 = get_time();
     while (1) {
-        t1 = get_time();
-        while ((winner = board_check_winner(&board, sets_to_win)) == PLAYER_NONE) {
-            t2 = get_time();
-            board_update(&board, t2 - t1);
-            // TODO: wait for button press
-            player = (player == PLAYER_1 ? PLAYER_2 : PLAYER_1);
-            t1 = t2;
+        switch (ISR_getState()) {
+            case PREPARA_INICIO:
+                // TODO: seta tela de inicio OLED
+                // TODO: habilita interrupt IRQA12
+                ISR_setState(INICIO);
+                break;
+            case INICIO:
+                // do nothing
+                break;
+            case LAUNCH_BALL:
+                board_reset_ball(&board);
+                ISR_setState(PLAYER_TURN);
+                break;
+            case PLAYER_TURN:
+                t2 = get_time();
+                board_update(&board, t2 - t1);
+                // TODO: wait for button press (IRQHandler)
+                player = (player == PLAYER_1 ? PLAYER_2 : PLAYER_1);
+                t1 = t2;
+            case LCD_UPDATE:
+                // TODO: update lcd with points
+                winner = board_check_winner(&board, sets_to_win);
+                ISR_setState(winner != PLAYER_NONE ? WIN_SCREEN : LAUNCH_BALL);
+                break;
+            case WIN_SCREEN:
+                // TODO: seta tela de vencedor OLED
+                ISR_setState(WIN_VISU);
+            case WIN_VISU:
+                // TODO: espera 5s
+                ISR_setState(PREPARA_INICIO);
+                break;
+            default:
+                break;
         }
-        // TODO: show winner screen
-        // TODO: play again?
     }
 }
 
@@ -55,4 +80,8 @@ player_t board_check_winner(board_t *board, uint8_t sets_to_win) {
         return PLAYER_2;
     }
     return PLAYER_NONE;
+}
+
+void board_reset_ball(board_t *board) {
+    // TODO
 }
