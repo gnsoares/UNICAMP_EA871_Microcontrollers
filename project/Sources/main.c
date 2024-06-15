11fab7a15d51243cedbd4bbdd9879e47adc460fa
jppascon <163413469+jppascon@users.noreply.github.com>
2024-06-15 12:32:34 -0300

    Update main.c

--- project/Sources/main.c
+++ project/Sources/main.c
@@ -5,11 +5,5 @@ int main(int argc, char const *argv[]) {
     config();
     // TODO: ask user for game config
     game_loop(2);
-
-    	    for (;;) {
-	                TPM_habilitaInterrupTOF(1);
-	                espera_2us(1500000);  // espera 3s para leitura
-	                TPM_desabilitaInterrupTOF(1);
-	                TPM_setaCnV(1, 1, 0);  // para sinal sonoro
     return 0;
 }
