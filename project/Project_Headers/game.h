98fd96dcd7f8373f7bdac104577fc42a12043c66
jppascon <163413469+jppascon@users.noreply.github.com>
2024-06-21 10:26:02 -0300

    Update game.h
    Adicionado headers das funções de plot de tela final de jogador

--- project/Project_Headers/game.h
+++ project/Project_Headers/game.h
@@ -55,5 +55,6 @@ void board_update_LCD_games(board_t *board);
 void board_update_LCD_points(board_t *board);
 void board_display(board_t *board);
 void board_startscreen_display();
+void board_pwins_display();
 
 #endif
